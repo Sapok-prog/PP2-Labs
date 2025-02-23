849923f7a9fe9ad7a126b290618a0147269f7a7f
Amina <sapokkot3@gmail.com>
2025-02-23 23:56:27 +0500

    last_commit

--- Labka5/RegEx/7.py
+++ Labka5/RegEx/7.py
@@ -1,7 +1,8 @@
 import re
 
 def to_camel_case(snake_case):
-    return re.sub(r'_' , '' , snake_case)
+    x = re.sub("_[a-z]" , lambda m : m.group(0)[1].upper(), snake_case)
+    return x
 
 snake_case = input()
 print(to_camel_case(snake_case))
